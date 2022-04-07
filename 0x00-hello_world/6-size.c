cce4dd8d815e0d59965f5202050c0019d358a16e
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-07 23:26:59 +0300

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-/* 
+
+/**
  * main - Entry point
  *
  * Return: ALways 0 (Success)
  */
-
 int main(void)
 {
-
-int intType;
-float floatType;	 
-double doubleType;
-char charType;
-
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of char: %zu byte\n", sizeof(charType));
-
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: %zu bytes\n", sizeof(a));
+printf("Size of an int: %zu bytes\n", sizeof(b));
+printf("Size of a long int: %zu bytes\n", sizeof(c));
+printf("Size of a long long int: %zu bytes\n", sizeof(d));
+printf("Size of a float: %zu bytes\n", sizeof(e));
 return (0);
 }
