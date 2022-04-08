47c306b0feedf694ce791eacc51890f869655512
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-09 01:46:41 +0300

    q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* something */
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+
+	putchar('\n');
+	return(0);
+}
