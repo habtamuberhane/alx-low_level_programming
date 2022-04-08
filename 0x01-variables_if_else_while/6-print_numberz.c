830145136efa555259a883a5f3a5c2ea60e9e4b8
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-09 02:20:55 +0300

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* something */
+
+/**
+ * main - nothing
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 10; i => 0; i--)
+	{
+		printf("%d", i);
+	}
+	printf('\n');
+
+	return (0);
+}
