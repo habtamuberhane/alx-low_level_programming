bd387f82670d155d0a6b26f983d32dacb7327667
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-09 01:14:42 +0300

    Print in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *main - Entry point
+ *
+ *Return: Alaways 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+	
+	for (ch='a'; ch <=z; ch++)
+	{
+		putchar("%d \n", ch);
+	}
+	
+	return (0);
+}
