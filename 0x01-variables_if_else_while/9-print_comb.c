f8996896869045074e483dc2eef850dbbdbccca1
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-09 02:48:31 +0300

    with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* Header for something */
+
+/**
+ *
+ * main - ENtry
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d, ", i);
+	}
+
+	printf("\n");
+	return (0);
+
+:wq
+
+
+}
