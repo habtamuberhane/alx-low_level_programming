e3bbc24a356e7d7b59998bedd9f82ec72b605377
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-09 01:51:07 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* something */
+
+/**
+ * Main
+ *
+ * Return
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf(i);
+	}
+
+	printf('\n');
+
+	return (0);
+}
