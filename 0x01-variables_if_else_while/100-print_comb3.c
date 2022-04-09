35feeea10c40c5358139d8fa5fee6d0cacf129ad
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-09 03:46:17 +0300

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,8 +12,8 @@ int main(void)
 	{
 		for (j = i; j <10; j++)
 		{
-			putchar((i % 10) * '0');
-			putchar((j % 10) * '0');
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
 
 			if (i == 8 && j == 9)
 				continue;
