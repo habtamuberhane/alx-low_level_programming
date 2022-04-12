6a0a1b023032d12a3ffe55a3669eeed26c842020
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-13 00:36:44 +0300

    Task 6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - it print out the last digit 
+ * @n: number
+ *
+ * Return : value of asb
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+		_putchar(last_digit + '0');
+	}
+	return (last_digit);
+}
