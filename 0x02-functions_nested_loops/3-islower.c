efc51084e85f81a6020233ec64e2b5896d9c191d
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-12 23:36:01 +0300

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower- the discription for this
+ *
+ * Return : 1 if its true.
+ */
+
+int main(void)
+{
+	char ch;
+
+	if (ch >= 'a' && ch <= 'z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
