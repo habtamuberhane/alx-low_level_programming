cd527a74f2c9fa33bcc054b26de1763350a9208c
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-12 23:53:57 +0300

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,19 @@
 /**
  * _islower - check if its lower case or not
  *
- *@c: discription of c 
+ *@c: discription of c
  *
  * Return: 1 if its true.
  */
 
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-return (1);
-else
-return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
