68f353a7e92b81a718803c0737c6b8f69b0cb2dc
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-13 01:42:15 +0300

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - bdubuacb dnjdv
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long i = 0, j = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = i + j;
+		if (sum > 4000000)
+		{
+			break;
+		}
+
+		if((sum % 2) == 0)
+		{
+			total_sum += sum;
+		}
+
+		i = j;
+		j = sum;
+	}
+	printf("%, of\n", total_sum);
+
+	return (0);
+}
