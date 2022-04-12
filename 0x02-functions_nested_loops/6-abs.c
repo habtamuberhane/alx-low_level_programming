fc3dc441565f1052bab666db832caaa4edb3eb99
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-13 00:27:53 +0300

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ * _abs - compute absolute value of the no
+ * #n: number
+ *
+ * Return: sdcbdjfvbkdf
+ */
+int _abs(int n)
+{
+	if(n >=0)
+		return (n);
+	else
+		return (-n);
+}
