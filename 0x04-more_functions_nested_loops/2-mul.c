a502708905ce53b9814073139b2e37d0b96c8fa8
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-15 18:10:44 -0400

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - Does the multiplication functio
+ * @a: value of number 1
+ * @b: value of number 2
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+	
+	c = a * b;
+	
+	printf("%d", c);
+
+	return (0);
+}
