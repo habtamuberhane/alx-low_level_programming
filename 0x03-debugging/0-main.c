0058a76357f4cf478c899cb31775fa48da821058
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-15 19:58:58 -0400

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests functions that the integer is +ve or -ve
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
