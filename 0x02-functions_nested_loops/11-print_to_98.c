9e52cc4df1d5137a018ffc2310d8b816050b7afb
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-17 12:58:22 +0300

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0;
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	i = 0;
+
+	do {
+		printf("%d", i);
+		i++;
+	}while(i == 98);
+
+	return (0);
+}
