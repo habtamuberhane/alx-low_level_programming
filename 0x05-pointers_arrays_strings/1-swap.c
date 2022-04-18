925564aae01a1e415d1804df2e146ab16d4bd477
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-18 19:25:13 -0400

    Swap Task 0

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -13,12 +13,11 @@ void swap_int(int *a, int *b)
 	*a = 98;
 	*b = 42;
 
-	printf("%d, ", *a);
-	printf("%d\n", *b);
+	printf("%d, %d\n", *b);
 
 	*a = *a - *b;
 	*b = *a + *b;
 	*a = *b - *a;
-	
+
 	printf("%d, %d\n", *a, *b);
 }
