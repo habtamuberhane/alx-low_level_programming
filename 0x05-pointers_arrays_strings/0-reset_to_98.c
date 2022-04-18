873667ee7d390ee02c8d6ba1760204ebae9a74d5
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-18 17:44:54 -0400

    Return to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,14 +7,16 @@
  * Return: Always 0.
  */
 
-void reset_to_98(int *n)
+int main()
 {
 	int n;
 
 	n = 402;
 
-	*n = 98;
-
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	
 	return (0);
 
 }
