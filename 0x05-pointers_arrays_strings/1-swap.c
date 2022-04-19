e36b109b9edfd8749c522c8960914036bdb8812d
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-18 20:02:47 -0400

    length of a string

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -15,12 +15,10 @@ void swap_int(int *a, int *b)
 	*a = 98;
 	*b = 42;
 
-	for (i = 0; i < 1; i++)
-	{
-		printf("%d, %d\n", *a, *b);
-		*a = *a - *b;
-		*b = *a + *b;
-		*a = *b - *a;
-		printf("%d, %d\n", *a, *b);
-	}
+	printf("%d, %d\n", *a, *b);
+	*a = *a - *b;
+	*b = *a + *b;
+	*a = *b - *a;
+	printf("%d, %d\n", *a, *b);
+
 }
