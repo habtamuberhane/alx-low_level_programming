05061d1e40c7562edee442dbaa967ad72ccd6729
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-21 00:31:58 +0300

    Task 1

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,19 +19,6 @@ char *_strcat(char *dest, char *src)
 	return (dest);
 }
 
-char *_strncat(char *dest, char *src, int n)
-{
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-	while (*src != '\0')
-	{
-		dest++;
-		src++;
-	}
-
-	return (dest);
-}
+char *_strncat(char *dest, char *src, int n);
 
 #endif
