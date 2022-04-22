dce321dd924f37c78a6cf9662531a677d7799653
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-22 23:53:22 +0300

    Reverse Array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main1 - check the code
+ * main - check the code
  * @a: an array of integers
  * @n: the number of elements to swap
  *
@@ -27,12 +27,12 @@ void print_array(int *a, int n)
 }
 
 /**
- * main1 - check the code
+ * main - check the code
  *
  * Return: Always 0.
  */
 
-int main1(void)
+int main(void)
 {
 	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 	
