1e161449bf7922631b3a74e1be33ae42d59154f2
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-24 13:06:19 +0300

    Task 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,22 +5,6 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void print_most_numbers(void)
-{
-	int i;
-	i = 0;
-
-	for (i = 0; i < 10; i++)
-	{
-		if (i == 2 || i == 4)
-		{
-			i++;
-		}
-		
-		putchar(i + '0');
-		putchar('\n');
-}
-
-
+void print_most_numbers(void);
 #endif
 
