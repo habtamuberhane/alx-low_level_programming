61e67c1beb456669a8fc7780b325c2776b7e322c
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-27 23:11:30 +0300

    Task 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,9 +7,9 @@
  * Return: The length of the string
  */
 
-int _strlen_recursion (char *s)
+int _strlen_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 		return (1 + _strlen_recursion(s + 1));
 
 	return (0);
@@ -21,7 +21,7 @@ int _strlen_recursion (char *s)
  * Return: Always 0
  */
 
-int main1 (void)
+int main1(void)
 {
 	int n;
 	
