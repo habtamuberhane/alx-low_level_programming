488dc43818ea450a6094ece2cf636816fa6c1b5a
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-27 17:07:03 +0300

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main1 - code
+ *
+ * Return - Always 0
+ */
+
+void _puts_recursion(char *s);
+
+int main1(void)
+{
+	puts_recursion("Puts with Recursion");
+
+	return (0);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,9 @@
+#define _MAIN_H_
+#ifdef _MAIN_H_
+
+void _puts_recursion(char *s)
+{
+	printf("%s", s);
+	return (s);
+}
+#endif
