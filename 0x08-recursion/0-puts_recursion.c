8affd9a039bdf7b61a12a1b573c4c4a3064ad787
habtamuberhane <habtamuberhanebb@gmail.com>
2022-04-27 20:04:48 +0300

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,16 @@
  * Return - Always 0
  */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_putchar_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
 
 int main(void)
 {
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,14 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-void _puts_recursion(char *s)
-{
-	if(*s)
-	{
-		putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		putchar('\n');
-}
+void _puts_recursion(char *s);
 #endif
