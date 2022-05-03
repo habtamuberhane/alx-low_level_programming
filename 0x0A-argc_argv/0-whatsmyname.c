3d9c414f4e3dda2362c4f325a8b17fd6e90e37b0
habtamuberhane <habtamuberhanebb@gmail.com>
2022-05-03 20:29:13 +0300

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main1 - the code that print the string
+ * main - the code that print the string
  * @argc: argument
  * @argv: argument value
  * Return: Always 0
  */
 
-int main1(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 		printf("%s\n", argv[0]);
