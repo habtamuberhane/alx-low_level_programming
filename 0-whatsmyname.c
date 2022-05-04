0822d9228906b44c5235f2d40ff11ec7a6c8fc55
habtamuberhane <habtamuberhanebb@gmail.com>
2022-05-04 19:45:32 +0300

    create_array

---
+++ 0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main1 = a code prints the name
+ * @argc: argument
+ * @argv: argument value
+ * Return: Always 0
+ */
+
+int main1(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0])
+	}
+
+	return (0);
+}
