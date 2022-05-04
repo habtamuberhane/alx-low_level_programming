02ddf28e03b71abc77c85652c2a928660ec0f935
habtamuberhane <habtamuberhanebb@gmail.com>
2022-05-04 23:01:49 +0300

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strdup - code that returns a pointer
+ * @str: parameter
+ * Return: Always 0 unless there is something unsuccessfull
+ */
+char *_strdup(char *str)
+{
+	char *new_str, *start;
+	int i = 0, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	start = str;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	str = start;
+	new_str = malloc(sizeof(char) * (len - 1));
+	start = new_str;
+
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for ( ; i < len; i++)
+		{
+			new_str[i] = *str;
+			str++;
+		}
+		new_str[i] = '\0';
+		return (start);
+	}
+}
