0ba80196aa33123b9b9a1a9fcc2e335f8b3b60b4
habtamuberhane <habtamuberhanebb@gmail.com>
2022-05-24 00:00:36 +0300

    print lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - A function that print a linked list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	printf("\n");
+	return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+#include <stdlib.h>
+
+/**
+ * struct listint_t - singly linked list
+ * @n: string
+ * @next: ponits to the next nodes
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+}listint_t;
+
+size_t print_listint(const listint_t *h);
+#endif
