e8a17583aa95143ed92b742a04ac034f635930fa
habtamuberhane <habtamuberhanebb@gmail.com>
2022-05-27 00:33:27 +0300

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - Function that gets bit at index
+ * @n: pointer 
+ * @index: index where the bit get
+ * Return: value of bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int max = 0x01;
+
+	max <<= index;
+	if (max == 0)
+		return (-1);
+
+	if ((n & max))
+		return (1);
+	else
+		return (0);
+}
