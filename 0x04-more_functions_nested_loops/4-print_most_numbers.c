10215e842d61fad3b17af9c6cf60bae770b49d44
habtamuberhane <habtamuberhanebb@gmail.com>
2022-06-16 22:03:25 +0300

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,21 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * print_most_numbers - check the code
  *
  * Return: always 0
  */
-
-
 void print_most_numbers(void)
 {
-	int i;
-	
-	i = 0;
+	int i = '0';
 
-	for (i = 0; i < 10; i++)
+	for (; i <= '9'; i++)
 	{
-		if (i == 2 || i == 4)
+		if (i != 2 && i != 4)
 		{
-			i++;
+			putchar(i);
 		}
-		_putchar(i + '0');
-		_putchar('\n');
 	}
+	putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+void print_most_numbers(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
